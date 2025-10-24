71ef77f3771e0b6acb08edaac83b9af716993fe5
greg <greg-github@lucidsolutions.co.nz>
2025-10-25 12:20:22 +1300

    upgrade `KeisukeYamashita/butane` provider to v0.1.4
    - fixes issue https://github.com/KeisukeYamashita/terraform-provider-butane/issues/14
    - see https://registry.terraform.io/providers/KeisukeYamashita/butane/latest

--- modules/vm/providers.tf
+++ modules/vm/providers.tf
@@ -8,18 +8,23 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = ">= 0.81.0"
+      version = ">= 0.85.1"
     }
 
-     /**
-        see:
-          - https://registry.terraform.io/providers/KeisukeYamashita/butane/latest
-      */
+    /**
+       see:
+         - https://registry.terraform.io/providers/KeisukeYamashita/butane/latest
+         - https://github.com/KeisukeYamashita/terraform-provider-butane
+     */
     butane = {
       source = "KeisukeYamashita/butane"
-      version = ">= 0.1.3"
+      version = ">= 0.1.4"
     }
 
+    /*
+      see:
+        - https://registry.terraform.io/providers/hashicorp/null/latest
+    */
     null = {
       source  = "hashicorp/null"
       version = ">= 3.2.4"
